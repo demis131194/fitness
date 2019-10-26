a8db72832d40ba12021000d70fe907be12610b1a
Demis1311 <demis131194@gmail.com>
2019-10-26 14:00:20 +0300

    Update script init mysql db.

--- src/main/resources/db/mysqlinit.sql
+++ src/main/resources/db/mysqlinit.sql
@@ -4,48 +4,95 @@ CREATE DATABASE IF NOT EXISTS fitness
 
 USE fitness;
 
+DROP TABLE IF EXISTS assignments;
 DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS trainers;
+DROP TABLE IF EXISTS admins;
 
-CREATE TABLE users
+CREATE TABLE admins
 (
-    id		 	 INT 							  NOT NULL AUTO_INCREMENT,
-    login	 	 VARBINARY(255) 				  NOT NULL UNIQUE,
+    id		 	 INT 							  UNSIGNED NOT NULL AUTO_INCREMENT,
+    login	 	 VARBINARY(255) 				  NOT NULL,
+    password 	 VARBINARY(255) 				  NOT NULL,
+    name	 	 VARCHAR(255) 					  NOT NULL,
+    lastName 	 VARCHAR(255) 					  NOT NULL,
+    role		 ENUM('ADMIN')                    NOT NULL DEFAULT 'ADMIN',
+    PRIMARY KEY (id),
+    UNIQUE KEY login_UNIQUE (login)
+);
+
+CREATE TABLE trainers
+(
+    id	 INT 							  NOT NULL AUTO_INCREMENT,
+    login	 	 VARBINARY(255) 				  NOT NULL,
     password 	 VARBINARY(255) 				  NOT NULL,
     name	 	 VARCHAR(255) 					  NOT NULL,
     lastName 	 VARCHAR(255) 					  NOT NULL,
     registerDate TIMESTAMP				          NOT NULL DEFAULT now(),
-    role		 ENUM('CLIENT','TRAINER','ADMIN') NOT NULL,
-    active		 BIT(1)                           NOT NULL DEFAULT 1,
-    PRIMARY KEY (id)
+    role		 ENUM('TRAINER')                  NOT NULL DEFAULT 'TRAINER',
+    phone		 VARCHAR(20)                      NOT NULL,
+    active		 BOOLEAN                          NOT NULL DEFAULT true,
+    PRIMARY KEY (id),
+    UNIQUE KEY login_UNIQUE (login)
+);
+
+CREATE TABLE clients
+(
+    id	 INT 							  NOT NULL AUTO_INCREMENT,
+    login	 	 VARBINARY(255) 				  NOT NULL,
+    password 	 VARBINARY(255) 				  NOT NULL,
+    name	 	 VARCHAR(255) 					  NOT NULL,
+    lastName 	 VARCHAR(255) 					  NOT NULL,
+    registerDate TIMESTAMP				          NOT NULL DEFAULT now(),
+    trainerId	 INT 							  NOT NULL,
+    role		 ENUM('CLIENT')                   NOT NULL DEFAULT 'CLIENT',
+    active		 BOOLEAN                          NOT NULL DEFAULT true,
+    discount     INT                              UNSIGNED NOT NULL DEFAULT 0 CHECK (discount >=0 AND discount <=100),
+    phone        VARCHAR(255)                     ,
+    PRIMARY KEY (id),
+    UNIQUE KEY login_UNIQUE (login),
+    FOREIGN KEY (trainerId) REFERENCES trainers (id)
 );
 
 CREATE TABLE orders
 (
-    id		 	INT 	NOT NULL AUTO_INCREMENT,
-    userId	 	INT 	NOT NULL,
-    startDate 	DATE 	NOT NULL,
-    endDate		DATE 	NOT NULL,
-    price		INT 	UNSIGNED NOT NULL,
-    pay 		BOOLEAN  NOT NULL DEFAULT false,
-    description TEXT 	,
-    PRIMARY KEY (id)    ,
-    FOREIGN KEY (userId) REFERENCES users (id)
+    id		 	    INT 	    NOT NULL AUTO_INCREMENT,
+    userId	 	    INT 	    NOT NULL,
+    trainerId	    INT 	    NOT NULL,
+    registerDate    TIMESTAMP   NOT NULL DEFAULT now(),
+    description     TEXT 	    ,
+    active          BOOLEAN     NOT NULL DEFAULT true,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES clients (id),
+    FOREIGN KEY (trainerId) REFERENCES trainers (id)
 );
 
-INSERT INTO users(login, password, name, lastName, registerDate, role)
-VALUES ('denis', 'denis', 'Денис', 'Кацук', '2014-01-10 13:00:00', 'admin'),
-       ('vasya', 'vasya','Вася', 'Василий', '2015-03-10 21:32:12', 'trainer'),
-       ('dima', 'dima','Дима', 'Дмитрий', '2016-07-16 15:14:00', 'client'),
-       ('vova', 'vova','Вова', 'Вован', '2016-08-19 17:52:42', 'client');
+CREATE TABLE assignments
+(
+    id		 	    INT 	    NOT NULL AUTO_INCREMENT,
+    orderId	 	    INT 	    NOT NULL,
+    registerDate    TIMESTAMP   NOT NULL DEFAULT now(),
+    exercises       TEXT 	    NOT NULL,
+    nutrition       TEXT 	    NOT NULL,
+    startDate       DATE 	    NOT NULL,
+    endDate         DATE 	    NOT NULL,
+    active          BOOLEAN     NOT NULL DEFAULT true,
+    price           INT         UNSIGNED NOT NULL,
+    accept          BOOLEAN     DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (orderId) REFERENCES orders (id)
+);
 
-INSERT INTO orders(userId, startDate, endDate, price, pay, description)
-VALUES (1, '2014-01-10', '2014-02-10', 200, true, 'None_1'),
-       (1, '2014-02-10', '2014-03-10', 210, true, 'None_1'),
-       (1, '2014-03-10', '2014-04-10', 220, false, 'None_1'),
-       (1, '2014-04-10', '2014-05-10', 225, false, 'None_1'),
-       (2, '2014-01-12', '2014-02-12', 150, true, 'None_2'),
-       (2, '2014-02-12', '2014-03-12', 150, true, 'None_2'),
-       (2, '2014-03-12', '2014-04-12', 150, true, 'None_2'),
-       (2, '2014-04-12', '2014-05-12', 150, false, 'None_2'),
-       (2, '2014-05-12', '2014-06-12', 160, false, 'None_2');
+CREATE TABLE comments
+(
+    id		 	    INT 	    NOT NULL AUTO_INCREMENT,
+    clientId	 	INT 	    NOT NULL,
+    trainerId	 	INT 	    NOT NULL,
+    registerDate    TIMESTAMP   NOT NULL DEFAULT now(),
+    comment         TEXT        NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (clientId) REFERENCES clients (id),
+    FOREIGN KEY (trainerId) REFERENCES trainers (id)
+);
